edeaf4bf7b686d34cdf4d2b21f5f9ac2326cb3fc
MadMaxGamer09 <madmaxgamer09@gmail.com>
2024-10-27 18:54:18 +0530

    initial commit

---
+++ CSharpTricks/CSharpTricks/Patterns/ConsolePatterns.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharpTricks.Patterns
+{
+    public class ConsolePatterns
+    {
+        public static void RunPattern()
+        {
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+            var random = new Random();
+
+            while (true)
+            {
+                Console.Clear();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    int x = random.Next(0, width);
+                    int y = random.Next(0, height);
+
+                    Console.SetCursorPosition(x, y);
+                    Console.Write("|");
+
+                    int x1 = random.Next(0, width);
+                    int y1 = random.Next(0, height);
+
+                    Console.SetCursorPosition(x1, y1);
+                    Console.Write("|");
+                    Console.ForegroundColor = (ConsoleColor)random.Next(0, 15);
+
+                    Task.Run(() => Console.Beep());
+                }
+
+                Thread.Sleep(60); // Delay for animation effect
+            }
+        }
+    }
+}
---
+++ CSharpTricks/CSharpTricks/Program.cs
@@ -0,0 +1,15 @@
+﻿using CSharpTricks.Patterns;
+
+namespace CSharpTricks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Console Pattern
+            ConsolePatterns.RunPattern();
+        }
+    }
+}
